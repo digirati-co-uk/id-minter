396a78caef82146db63efb28fb83fc7a7823efe6
tomcrane <tom@nanoplanet.com>
2024-02-13 10:56:30 +0000

    dotnet id minter

---
+++ dotnet/IdMinter/IdMinter/Identifiable.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace IdMinter
+{
+    public class Identifiable
+    {
+        private static readonly char[] Numbers = "23456789".ToCharArray();                   // not 1
+        private static readonly char[] Letters = "abcdefghjkmnpqrstuvwxyz".ToCharArray();    // not i, l, o
+        private static readonly char[] All = [.. Numbers, .. Letters];
+
+        public static string Generate(int length = 8, bool letterFirst = true)
+        {
+            Random random = new Random();
+            var sb = new StringBuilder(length);
+            for(int i = 0; i < length; i++)
+            {
+                if(letterFirst && i==0)
+                {
+                    sb.Append(Letters[random.Next(Letters.Length)]);
+                }
+                else
+                {
+                    sb.Append(All[random.Next(All.Length)]);
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ dotnet/IdMinter/IdMinterDemo/Program.cs
@@ -0,0 +1,23 @@
+﻿using IdMinter;
+
+Console.WriteLine("Identifiable.Generate()");
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(Identifiable.Generate());
+}
+Console.WriteLine();
+
+Console.WriteLine("Identifiable.Generate(length:16)");
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(Identifiable.Generate(length: 16));
+}
+Console.WriteLine();
+
+Console.WriteLine("Identifiable.Generate(letterFirst:false)");
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(Identifiable.Generate(letterFirst: false));
+}
+Console.WriteLine();
+
